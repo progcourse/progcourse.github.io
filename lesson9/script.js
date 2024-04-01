0b7b6c6a51de1dd356d7117843c01bfc76347553
Seryog Kirakosyan <yeghegnadzorprogrammers@gmail.com>
2024-04-01 13:50:00 +0400

    Lesson 9

---
+++ lesson9/script.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let number1 = parseFloat(prompt("Enter first number:"));
+let operator = prompt("Enter operator (+, -, *, /, %):");
+let number2 = parseFloat(prompt("Enter second number:"));
+
+let result;
+switch (operator) {
+    case '+':
+        result = number1 + number2;
+        break;
+    case '-':
+        result = number1 - number2;
+        break;
+    case '*':
+        result = number1 * number2;
+        break;
+    case '/':
+        result = number1 / number2;
+        break;
+    case '%':
+        result = number1 % number2;
+        break;
+    default:
+        alert("Invalid operator");
+        break;
+}
+
+alert("Result: " + result);
+*/
